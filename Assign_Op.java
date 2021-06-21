44349935d7e6da991b5bc0bcc3fb96697ab8b439
Shanku Natra <85866869+Shanku-Natra@users.noreply.github.com>
2021-06-20 23:10:56 -0300

    Add files via upload

---
+++ Assign_Op.java
@@ -0,0 +1,12 @@
+package Operators;
+public class Assignment_Op {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int c = 20;			 
+		 c += b;  // c = c+b; // 25
+		 System.out.println(c);	
+		 c -= b;  // c = c-b; // 20
+		 System.out.println(c);	
+	}
+}

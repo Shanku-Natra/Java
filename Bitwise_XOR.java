36c9c7ca9ac00e8612d8fd6b0314d208da043cdb
Shanku Natra <85866869+Shanku-Natra@users.noreply.github.com>
2021-06-20 23:18:07 -0300

    Add files via upload

---
+++ Bitwise_XOR.java
@@ -0,0 +1,11 @@
+package Operators;
+public class Bitwise_XOR {
+	public static void main(String[] args) {
+		int a=5;
+		int b=10;
+		int c=15;
+		System.out.println(a^b);
+		System.out.println(b^c);
+		System.out.println(a^b^c);
+	}
+}

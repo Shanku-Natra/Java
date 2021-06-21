2ff357961728144957ed12bd489d47e155417920
Shanku Natra <85866869+Shanku-Natra@users.noreply.github.com>
2021-06-20 23:15:13 -0300

    Add files via upload

---
+++ Bitwise_AND.java
@@ -0,0 +1,11 @@
+package Operators;
+public class Bitwise_AND {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println(a&b);
+		System.out.println(a & a);
+		System.out.println(b&b);
+	}
+
+}

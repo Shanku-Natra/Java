0eeab67c2bc1b93a91e25de0aae91b8e8fb456a6
Shanku Natra <85866869+Shanku-Natra@users.noreply.github.com>
2021-06-20 23:16:42 -0300

    Add files via upload

---
+++ Bitwise_OR.java
@@ -0,0 +1,12 @@
+package Operators;
+public class Bitwise_OR {
+	public static void main(String[] args) {
+		int a=5;
+		int b=15;
+		int c=20;
+		System.out.println(a|b);
+		System.out.println(a|c);
+		System.out.println(b|c);
+		System.out.println(a|b|c);
+}
+}

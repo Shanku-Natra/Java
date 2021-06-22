a58e539722c52954b3663491163e072ab706e9ea
Shanku Natra <85866869+Shanku-Natra@users.noreply.github.com>
2021-06-21 23:22:10 -0300

    Add files via upload

---
+++ Ternary_Op.java
@@ -0,0 +1,9 @@
+package Operators;
+public class Ternary_Op {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;			 
+		int c =( (a<b) ? a : b );
+		System.out.println(c);			 
+	}
+}

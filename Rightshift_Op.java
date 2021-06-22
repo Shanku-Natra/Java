03951e714ad0ea4ad17300de9892436f74d17363
Shanku Natra <85866869+Shanku-Natra@users.noreply.github.com>
2021-06-21 23:26:30 -0300

    Add files via upload

---
+++ Rightshift_Op.java
@@ -0,0 +1,9 @@
+package Operators;
+public class Rightshift_Op {
+	public static void main(String[] args) {
+		System.out.println(10>>>2); // 10/2^2 = 10/4 = 2
+		System.out.println(20>>2); // 20/2^2 = 20/4 = 5
+		System.out.println(20>>>2); // 20/2^2 = 20/4 = 5
+	}
+
+}

e58a167eae7155bcaa1ab55590db41bf6c090c51
Shanku Natra <85866869+Shanku-Natra@users.noreply.github.com>
2021-06-21 23:20:41 -0300

    Add files via upload

---
+++ Logical_Op.java
@@ -0,0 +1,13 @@
+package Operators;
+public class Logical_Op {
+	public static void main(String[] args) {
+	int a = 10;
+	int b = 5;
+	int c = 20;
+	System.out.println(a<b && a<c); // false
+	System.out.println(a>b && a<c); // true
+	System.out.println(a<b || a<c); // true
+	System.out.println(a<b || a>c); // false
+	System.out.println(a>b || a<c); // true
+	}
+}

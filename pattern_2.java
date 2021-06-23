0c01fb0719f67510560aec3145f1aaae511bdc23
Shanku Natra <85866869+Shanku-Natra@users.noreply.github.com>
2021-06-23 10:37:28 -0300

    Add files via upload

---
+++ pattern_2.java
@@ -0,0 +1,12 @@
+package Patterns;
+public class pattern_2 {
+	public static void main(String[] args) {
+		for(int i = 1; i<=5;i++) // 5 rows - 1,2,3,4,5
+		{
+		for (int j =1; j<=5; j++) // 5 times
+		{
+			System.out.print(i);
+		}
+		System.out.println();
+		}
+	} }

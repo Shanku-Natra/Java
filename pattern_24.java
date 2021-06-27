fd7b4edf616e80521a3d5de8a1e4321a5fd3fa07
Shanku Natra <85866869+Shanku-Natra@users.noreply.github.com>
2021-06-27 12:09:00 -0300

    Add files via upload

---
+++ pattern_24.java
@@ -0,0 +1,14 @@
+package Patterns;
+public class pattern_24 
+{
+	public static void main(String[] args)
+	{
+	for(int i =5; i>=1;i--)
+	{
+    for(int j=5; j>=i;j--)
+    {
+    	System.out.print((char)(i+64)+" ");
+    }
+    System.out.println();
+	}
+}}
